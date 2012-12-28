cd04d7cedadde081c48a6d3e3f159c175b964b91
David Anguita <davidbanguita@gmail.com>
2012-12-28 19:34:34 +0100

    Add size param to Gravatar image URI

--- lib/default.rb
+++ lib/default.rb
@@ -3,7 +3,8 @@
 
 require "digest/md5"
 
-def gravatar(email_address, size = nil)
-  hash = Digest::MD5.hexdigest(email_address)
-  "http://www.gravatar.com/avatar/#{hash}"
+def gravatar email_address, size = 80
+  hash = Digest::MD5.hexdigest(email_address.downcase)
+
+  "http://www.gravatar.com/avatar/#{hash}?s=#{size}"
 end
