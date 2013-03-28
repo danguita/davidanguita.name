3c753c55a750ad2f901221d482a2ea28b16a8064
David Anguita <david@davidanguita.name>
2013-03-28 11:37:55 +0100

    Refactor: Move custom helpers into a new lib file

--- config.rb
+++ config.rb
@@ -60,35 +60,9 @@ activate :automatic_image_sizes
 #   end
 # end
 
-helpers do
-  def draw_page_title
-    [data.settings.site.name, data.page.title].reject(&:blank?).join(': ')
-  end
-
-  def draw_page_nav
-    markup = []
-
-    %w(services methodology contact).each do |page|
-      markup << link_to(page_title_map[page.to_sym], "/#{page}", :class => ['btn btn-primary', page_status(page)].join(' '))
-    end
-
-    markup.join(' ')
-  end
-
-  def page_title_map
-    { :services => 'Servicios', :methodology => 'Metodología', :contact => 'Contacto' }
-  end
-
-  def page_status page
-    (data.page.item === page) ? 'active' : 'inactive'
-  end
-
-  def gravatar_for email_address, size = 80
-    hash = Digest::MD5.hexdigest(email_address.downcase)
-
-    "http://www.gravatar.com/avatar/#{hash}?s=#{size}"
-  end
-end
+# CustomHelpers
+require "lib/custom_helpers"
+helpers CustomHelpers
 
 set :css_dir, 'stylesheets'
 
