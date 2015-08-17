57d0feddccc65a8dfa09d63bfabedf8648a53b06
David Anguita <david@davidanguita.name>
2015-08-17 17:37:12 +0200

    Better assets organization

--- config.rb
+++ config.rb
@@ -1,28 +1,20 @@
-###
-# Global settings
-###
-
-activate :i18n, :mount_at_root => :en
-
+activate :i18n, mount_at_root: :en
 activate :directory_indexes
-
-###
-# Compass
-###
-
-# Change Compass configuration
-compass_config do |config|
- config.output_style = :compact
-end
-
-###
-# Page options, layouts, aliases and proxies
-###
+activate :automatic_image_sizes
 
 # Dynamic pages: Projects
 data.projects.en.each do |key, _|
-  proxy "/projects/#{key}.html", "/localizable/projects/template.html", :locals => { :key => key }, :lang => :en, :ignore => true
-  proxy "/es/projects/#{key}.html", "/localizable/projects/template.html", :locals => { :key => key }, :lang => :es, :ignore => true
+  proxy "/projects/#{key}.html",
+    "/localizable/projects/template.html",
+    locals: { :key => key },
+    lang: :en,
+    ignore: true
+
+  proxy "/es/projects/#{key}.html",
+    "/localizable/projects/template.html",
+    locals: { :key => key },
+    lang: :es,
+    ignore: true
 end
 
 # Ignore proxied templates
@@ -30,10 +22,10 @@ ignore "projects/template.html"
 ignore "es/projects/template.html"
 
 # Sitemap
-page "/sitemap.xml", :layout => false
+page "/sitemap.xml", layout: false
 
-# HTTP 404 Not Found (I really want the extension)
-page "/404.html", :directory_index => false
+# HTTP 404 Not Found (I do want the extension)
+page "/404.html", directory_index: false
 ignore "es/404.html"
 
 # Ignore content files (markdown)
@@ -43,26 +35,25 @@ ignore "content/*"
 ignore "stylesheets/vendor/*"
 ignore "stylesheets/modules/*"
 
-###
-# Helpers
-###
-
-# Automatic image dimensions on image_tag helper
-activate :automatic_image_sizes
-
 # CustomHelpers
 require "lib/custom_helpers"
 helpers CustomHelpers
 
-set :css_dir, 'stylesheets'
+# Assets
+set :css_dir, "assets/stylesheets"
+set :js_dir, "assets/javascripts"
+set :fonts_dir, "assets/fonts"
+set :images_dir, "assets/images"
+set :partials_dir, "partials"
 
-set :js_dir, 'javascripts'
-
-set :images_dir, 'images'
+# Compass config
+compass_config do |config|
+ config.output_style = :compact
+end
 
 # Build-specific configuration
 configure :build do
-  # For example, change the Compass output style for deployment
+  # Change the Compass output style for deployment
   activate :minify_css
 
   # Minify Javascript on build
